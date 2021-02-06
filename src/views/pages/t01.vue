eff7471d39828bf78e96b3aaea834e5ac944c664
LayMui <laymui.toh@taiger.com>
2021-02-06 20:39:12 +0800

    Add part 2 on Tech debts

---
+++ src/views/pages/t01.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-center">
+    <img src="../../assets/image/t01.png"/>
+
+    
+  </div>
+</template>
---
+++ src/views/pages/t05.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-center">
+    <img src="../../assets/image/t05.png"/>
+
+    
+  </div>
+</template>
